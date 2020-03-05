7988acba4ba5a9939011acbc758e07e7176e2e5d
LAPTOP-Q0L54CT7\Sugyan Subedi <sujit.subedi@selu.edu>
2020-03-04 22:32:04 -0600

    Creating a React Table

---
+++ client/src/components/EmployeeTable.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import EmployeeTableRow from './EmployeeTableRow';
+
+const EmployeeTable= (props)=>{
+    return(
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First Name</th>
+      <th scope="col">Last Name</th>
+      <th scope="col">Job Title</th>
+      <th scope="col">Salary</th>
+    </tr>
+  </thead>
+  <tbody>
+   
+  </tbody>
+</table>
+    )
+}
