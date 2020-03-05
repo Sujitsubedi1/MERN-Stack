25bc9b964cd69520c063337b341dc9c1c6e109a8
LAPTOP-Q0L54CT7\Sugyan Subedi <sujit.subedi@selu.edu>
2020-03-04 23:07:11 -0600

    Final Changes

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ app.use(bodyParser.json());
 const employee = require ('./routes/employee');
 app.use('/employee', employee);
 
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+  app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'client','build','index.html'));
+  })
+}
+
+
+
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://Sujitsubedi1:America1@cluster0-gsedg.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
